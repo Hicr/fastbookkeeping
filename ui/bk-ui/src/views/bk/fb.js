9a0d8fa47ff90f81b31292e98339a863959a6fc2
wangcr <1q6av4b@gmail.com>
2023-01-29 15:40:34 +0800

    #2023-1-29 #wangcr #Update: 更新类型标签

--- ui/bk-ui/src/views/bk/fb.js
+++ ui/bk-ui/src/views/bk/fb.js
@@ -1,12 +1,12 @@
 const FB_TYPE = [
-  {label:'交通'},{label:'购物'},{label:'生活'},{label:'社交'},{label:'饮食'}
+  {label:'交通'},{label:'购物'},{label:'生活'},{label:'社交'},{label:'饮食'},{label:'宠物'}
 ]
 
 const FB_TAGS = [
   {label:'早餐 8元',value:'饮食/早餐/8',color:''},
-  {label:'交通 15元',value:'交通/交通/15',color:''},
-  {label:'交通 9元',value:'交通/交通/9',color:''},
-  {label:'午餐 14元',value:'饮食/午餐/14',color:''}
+  {label:'打车 15元',value:'交通/打车/15',color:''},
+  {label:'打车 9元',value:'交通/打车/9',color:''},
+  {label:'话费 200元',value:'生活/话费/200',color:''}
 ]
 
 export {
