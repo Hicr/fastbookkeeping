9b070af771be4aad76f10cc8b55266ca7fcffef5
wangcr <1q6av4b@gmail.com>
2023-02-24 10:21:27 +0800

    # 添加分类

--- ui/bk-ui/src/views/bk/fb.js
+++ ui/bk-ui/src/views/bk/fb.js
@@ -1,12 +1,13 @@
 const FB_TYPE = [
-  {label:'交通'},{label:'购物'},{label:'生活'},{label:'社交'},{label:'饮食'},{label:'宠物'}
+  {label:'交通'},{label:'购物'},{label:'生活'},{label:'饮食'},{label:'宠物'},{label:'游戏'}
 ]
 
 const FB_TAGS = [
   {label:'早餐 8元',value:'饮食/早餐/8',color:''},
   {label:'打车 15元',value:'交通/打车/15',color:''},
   {label:'打车 9元',value:'交通/打车/9',color:''},
-  {label:'话费 200元',value:'生活/话费/200',color:''}
+  {label:'话费 200元',value:'生活/话费/200',color:''},
+  {label:'交通卡 100元',value:'交通/地铁卡/100',color:''}
 ]
 
 export {
